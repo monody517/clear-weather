cbaa327e8601dbac039d3a97e7741eea6569685f
wenpei <2737716714@qq.com>
2022-07-25 14:10:20 +0800

    字体引入成功

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,41 +1,61 @@
 <template>
   <view>
-    <text class="title">{{ counter.count }}</text>
-    <view class="button" @tap="onAdd">ADD</view>
+    <text class="title">{{contentRef }}</text>
   </view>
 </template>
 
 <script>
-import { useCounterStore } from '../stores/counter'
+
+import Taro from '@tarojs/taro'
+import {ref,onMounted} from 'vue'
+
+const jinrishici = require('../../src/utils/jinrishici.js')
 
 export default {
   setup() {
-    const counter = useCounterStore()
+    const contentRef = ref('')
 
-    const onAdd = () => {
-      counter.count++
+    onMounted(()=>{
+      jinrishici.load(result => {
+        contentRef.value = result.data.content
+        const contentArr = contentRef.value.split('，')
+      })
+      Taro.loadFontFace({
+        family: 'yingqu',
+        source: 'https://qingyu-1313022355.cos.ap-chengdu.myqcloud.com/LXGWWenKai-Bold.ttf',
 
-      // with autocompletion ✨
-      // counter.$patch({count: counter.count + 1})
-
-      // or using an action instead
-      // counter.increment()
-    }
+        success(res){
+          console.log('res', res)
+        },
+        fail(err){
+          console.log('err', err)
+        },
+      })
+    })
 
     return {
-      counter,
-      onAdd
+      contentRef,
     }
   }
 }
+
+
 </script>
 
 <style>
 .title {
-  font-size: 32px;
+  font-family: 'yingqu';
+  font-size: 50px;
+  writing-mode: vertical-lr;/*从左向右 从右向左是 writing-mode: vertical-rl;*/  
+  writing-mode: tb-lr;/*IE浏览器的从左向右 从右向左是 writing-mode: tb-rl；*/  
 }
 .button {
   border: 1px solid lightgray;
   padding: 5px 10px;
 }
 </style>
+
+
+
+
+
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,53 +1,18 @@
 <template>
   <view class="index">
-  <view class="content">{{contentRef}}</view>
-  <view @click="loadFont">dianji</view>
+  <Counter/>
   </view>
 </template>
 
 <script>
 import './index.scss'
 import Counter from '../../components/Counter.vue'
-import {ref,onMounted} from 'vue'
-import Taro from '@tarojs/taro'
-
-const jinrishici = require('../../utils/jinrishici.js')
 
 export default {
   name: 'Index',
 
-  components: {},
+  components: {Counter},
 
-  setup(){
-    const contentRef = ref('')
-    onMounted(()=>{
-      jinrishici.load(result => {
-        contentRef.value = result.data.content
-      })
-    })
-    return {
-        contentRef
-    }
-  },
 
-  methods: {
-    loadFont() {
-      console.log('111');
-      Taro.loadFontFace({
-      family: 'yingqu',
-      source: 'https://qingyu-1313022355.cos.ap-chengdu.myqcloud.com/LXGWWenKai-Bold.ttf',
-
-      success(res){
-        console.log('res', res)
-      },
-      fail(err){
-        console.log('err', err)
-      },
-      complete(res){
-        console.log('complete', res)
-      },
-    })
-    }
-  }
 }
 </script>
