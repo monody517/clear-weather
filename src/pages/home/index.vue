97f34f4012b14b4bf436cbc4ccce7ac5a182790e
wenpei <2737716714@qq.com>
2022-07-26 20:13:34 +0800

    图片改为云端获取

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,89 +1,84 @@
 <template>
   <view class="index">
-    <view v-for="(item,index) of imgList" :key="index">
-      <view>{{`../images/${item.imgUrl}`}}</view>
-      <image v-if="item.imgUrl" :src="`../images/${item.imgUrl}`" alt=""/>
-    </view>
-    <HomeTab :currentCity="currentCity"/>
-    <Content/>
-    <Weather :latitude="latitude" :longitude="longitude"/>
+    <!-- <image :src="`https://qingyu-1313022355.cos.ap-chengdu.myqcloud.com/${'qing.jpg'}`" alt=""/> -->
+    <HomeTab :currentCity="currentCity" />
+    <Content />
+    <Weather :latitude="latitude" :longitude="longitude" />
   </view>
 </template>
 
 <script lang="ts">
-import './index.scss'
-import Content from '../home/components/Content.vue'
-import HomeTab from '../home/components/homeTab.vue'
-import Weather from '../home/components/weather.vue'
-import {ref,onMounted, reactive} from 'vue'
-import Taro from '@tarojs/taro'
+import "./index.scss";
+import Content from "../home/components/Content.vue";
+import HomeTab from "../home/components/homeTab.vue";
+import Weather from "../home/components/weather.vue";
+import { ref, onMounted, reactive } from "vue";
+import Taro from "@tarojs/taro";
 
-const QQMapWX = require('../../utils/qqmap-wx-jssdk.js')
+const QQMapWX = require("../../utils/qqmap-wx-jssdk.js");
 
 export default {
-  name: 'Index',
+  name: "Index",
 
-  components: {Content,HomeTab,Weather},
+  components: { Content, HomeTab, Weather },
 
   setup() {
-    
-    const currentCity = ref('')
-    const latitude = ref()
-    const longitude = ref()
-    const imgList=reactive([
+    const currentCity = ref("");
+    const latitude = ref();
+    const longitude = ref();
+    const imgList = reactive([
       {
-        imgUrl: 'qing.jpg',
-        key: 'qing'
+        imgUrl: "qing.jpg",
+        key: "qing",
       },
       {
-        imgUrl: 'yu.jpg',
-        key: 'yu'
+        imgUrl: "yu.jpg",
+        key: "yu",
       },
       {
-        imgUrl: 'yun.jpg',
-        key: 'yun'
+        imgUrl: "yun.jpg",
+        key: "yun",
       },
       {
-        imgUrl: 'xue.jpg',
-        key: 'xue'
+        imgUrl: "xue.jpg",
+        key: "xue",
       },
-    ])
+    ]);
 
-    onMounted(()=>{
+    onMounted(() => {
       const qqmapsdk = new QQMapWX({
-          key: 'GZOBZ-STL3J-WXOFV-FDSNH-UPDKV-DQFCH'
+        key: "GZOBZ-STL3J-WXOFV-FDSNH-UPDKV-DQFCH",
       });
 
-    // 获取用户经纬度
+      // 获取用户经纬度
       Taro.getLocation({
-        type: 'wgs84',
+        type: "wgs84",
         success: function (result) {
-              // 获取用户城市
+          // 获取用户城市
           qqmapsdk.reverseGeocoder({
             location: {
-            latitude: result.latitude.toString(),
-            longitude: result.longitude.toString()
+              latitude: result.latitude.toString(),
+              longitude: result.longitude.toString(),
             },
             success: async function (res) {
-              let city = res.result.ad_info.city 
-              currentCity.value = city
-              latitude.value = result.latitude
-              longitude.value = result.longitude
+              let city = res.result.ad_info.city;
+              currentCity.value = city;
+              latitude.value = result.latitude;
+              longitude.value = result.longitude;
             },
             fail: function (res) {
               console.log(res);
             },
-          });  
-        }
-      })
-  })
-  return {
-    currentCity,
-    latitude,
-    longitude,
-    imgList,
-  }
-}
-
-}
+          });
+        },
+      });
+    });
+    return {
+      currentCity,
+      latitude,
+      longitude,
+      imgList,
+    };
+  },
+};
 </script>
